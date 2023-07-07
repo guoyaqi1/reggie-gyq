1306ddf1b40b6fd8f2c7de97b9a0d9cc00938dc9
guoyaqi <http://mail.21cn.net/>
2023-07-07 22:21:27 +0800

    使用Spring Cache来缓存菜品套餐

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -3,11 +3,14 @@ package com.itheima.reggie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @ServletComponentScan
 @SpringBootApplication
 @EnableTransactionManagement //开启事务支持的注解
+@EnableCaching  //开启cache缓存
+
 public class ReggieApplication {
     public static void main(String[] args){
         SpringApplication.run(ReggieApplication.class,args);
